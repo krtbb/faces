0cd29114ea0fdf9223d9d82f9c4eefb6695fb30a
krtbb <auri.tk2009@gmail.com>
2020-12-05 08:33:09 +0000

    added pairwise_loss, triplet_loss

--- utils/losses.py
+++ utils/losses.py
@@ -12,3 +12,32 @@ def negative_log_likelihood(x_mu, x_sigma, y, batchsize=1, min_variance=1e-6, se
                np.sum( 0.5 * np.log(x_sigma_) ) / batchsize
     else:
         return np.sum( 0.5 * (diff*diff*x_sigma_recip + np.log(x_sigma_))) / batchsize
+
+def pairwise_loss(x, y, equal=True, epsilon=1e+5, dist_method=sum_square):
+    """
+    *args*
+    x --> type=np.ndarray, predicted vector
+    y --> type=np.ndarray, predicted vector
+    equal --> type=bool, means that x and y is the same person.
+    epsilon --> type=float, a margin for not-equal samples.
+
+    *return*
+    type=np.float32, loss values
+    """
+    if equal:
+        return dist_method(x, y)
+    else:
+        return np.max(0, epsilon - dist_method(x, y) )
+
+def triplet_loss(x, p, n, epsilon=1e+5, dist_method=sum_square):
+    """
+    *args*
+    x --> type=np.ndarray, predicted vector
+    p --> type=np.ndarray, predicted vector from the same person with `x`
+    n --> type=np.ndarray, predicted vector from a different person with `x`
+    epsilon --> type=float, a margin.
+
+    *return*
+    type=np.float32, loss values
+    """
+    return np.max(0, epsilon + dist_method(x, p) - dist_method(x, ns))
