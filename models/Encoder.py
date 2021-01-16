f4c7234954e56a90946c91f40cf18c9853c51fb7
krtbb <auri.tk2009@gmail.com>
2021-01-16 07:51:50 +0000

    init

--- models/Encoder.py
+++ models/Encoder.py
@@ -4,9 +4,32 @@ import os
 import sys
 import tensorflow as tf
 
+from glob import glob
+
+from models.apps import FaceModel
+from utils.analysis import load_model
+from utils.generals import load_json
+
 class Encoder(object):
-    def __init__(self, log_dir):
+    def __init__(self, log_dir, epoch=-1):
         self.log_dir = log_dir
         
-        savedmodel_path = os.path.join(self.log_dir, 'model.saved_model')
-        self.model = tf.saved_model.load(savedmodel_path)
+        # load configs
+        json_path = os.path.join(self.log_dir, 'train_config.json')
+        saved_params_paths = sorted(glob(os.path.join(self.log_dir, 'model_*.h5')))
+        if epoch == -1:
+            saved_params_paths = sorted(glob(os.path.join(self.log_dir, 'model_*.h5')))
+            saved_params_path = saved_params_paths[-1]
+        elif epoch >= 0:
+            saved_params_path = os.path.join(self.log_dir, 'model_{}.h5'.format(epoch))
+        
+        # restore models
+        self.model = load_model(json_path)
+        self.model.load_weights(saved_params_path)
+        self.insize = self.model.input_shape[1]
+        self.outsize = self.model.output_shape[1]
+
+    def __call__(self, imgs, batchsize=None, verbose=0, use_multiprocessing=False):
+        return self.model.predict(imgs, batch_size=batchsize, verbose=verbose, use_multiprocessing=use_multiprocessing)
+
+        
