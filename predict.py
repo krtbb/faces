d72c83c899d1d52591e660a131c91933c0e0c8be
krtbb <auri.tk2009@gmail.com>
2021-01-16 07:51:59 +0000

    init

--- predict.py
+++ predict.py
@@ -15,10 +15,21 @@ tf.enable_eager_execution(config=config)
 from glob import glob
 from tqdm import tqdm
 
-from models.micro import FirstCNN
-from models.apps import FaceModel, app_net
+from models.Encoder import Encoder
 
 from utils.losses import pairwise_loss, triplet_loss
 from utils.preparations import channelSwap, normalize, resize
 from utils.preprocess import load_and_preprocess_image
 
+def predict(log_dir, epoch):
+    # prepare model
+    if 'logs' in log_dir:
+        encoder = Encoder(log_dir, epoch=-1)
+    else:
+        raise ValueError('Invalid log_dir(`logs` is not in log_dir')
+
+    # prepare dataset
+
+    resized = cv2.resize(img, (encoder.insize, encoder.insize))
+    
+    # execute evaluation
